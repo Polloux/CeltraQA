bc9144f4ad302786983cbfd46d06f2b90e0769e1
Polloux <49200245+Polloux@users.noreply.github.com>
2025-06-12 17:13:21 +0200

    Init commit

---
+++ codecept.conf.js
@@ -0,0 +1,17 @@
+/** @type {CodeceptJS.MainConfig} */
+exports.config = {
+  tests: './tests/*_test.js',
+  output: './output',
+  helpers: {
+    Playwright: {
+      browser: 'chromium',
+      url: 'http://localhost',
+      show: true, // Not headless?
+      waitForNavigation: "domcontentloaded"
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  name: 'CeltraQA'
+}
